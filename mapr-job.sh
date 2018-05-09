bf75cc55f319bafb14c7dee441f6b534dec25137
Chad Smykay <csmykay@maprtech.com>
2018-05-09 18:35:52 +0000

    Update 'mapr-job.sh'

--- mapr-job.sh
+++ mapr-job.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+# We need to ensure that the cluster is up and running before we start executing any scripts to setup a demo.
 # Below loads all of the default "MapR" enviroment variables for the SE Demo cluster.
 source /opt/mapr/docker/start-env.sh
-# We need to ensure that the cluster is up and running before we start executing any scripts to setup a demo.
+# By default our enviroment variables do not have our MCS port.
+MCS_PORT=${MCS_PORT:-8443}
 MCS_URL="https://${MCS_HOST}:${MCS_PORT}"
-
 echo ""
 echo "MapR JOB script executing for demo, Centralized Historian."
 echo ""
 chk_str="Waiting ..."
 # Check that the CLDB is up and running.
 check_cluster(){
-        find_cldb="curl -sSk -u ${MAPR_ADMIN}:${MAPR_ADMIN_PASSWORD} ${MCS_URL}/rest/node/cldbmaster"
+        find_cldb="curl -sSk -u ${MAPR_ADMIN}:${MAPR_ADMIN_PASS} ${MCS_URL}/rest/node/cldbmaster"
         if [ "$($find_cldb | jq -r '.status')" = "OK" ]; then
                 return 0
         else
