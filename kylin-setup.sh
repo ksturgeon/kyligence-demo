0d1089eaf1c1f234b0ff09d4fa0eba8790ac6452
Kevin Sturgeon <ksturgeon@gmail.com>
2019-03-26 11:46:39 -0400

    fixed chown

--- kylin-setup.sh
+++ kylin-setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 #Copy kyligence tarball from public data
-#sudo cp /public_data/kyligence/install/Kyligence-Enterprise-3.2.2.2028-GA-mapr.tar.gz /opt
 sudo tar -zxvf /public_data/kyligence/install/Kyligence-Enterprise-3.2.2.2028-GA-mapr.tar.gz -C /opt/
-sudo chown -R mapr:mapr /opt/Kyligence-Enterprise-3.2.2.2028-GA-mapr.tar.gz
+sudo chown -R mapr:mapr /opt/Kyligence-Enterprise-3.2.2.2028-GA-mapr
 
 #Create prereqs
 export KYLIN_HOME=/opt/Kyligence-Enterprise-3.2.2.2028-GA-mapr
@@ -15,7 +14,7 @@ rm -f $KYLIN_HOME/conf/profile
 ln -sfn $KYLIN_HOME/conf/profile_min $KYLIN_HOME/conf/profile
 
 
-
+#Add properties
 echo "kylin.env.zookeeper-connect-string=mapr-zk:5181" >> $KYLIN_HOME/conf/kylin.properties
 echo "kylin.env.hdfs-working-dir=maprfs:///kylin" >> $KYLIN_HOME/conf/kylin.properties
 echo "kylin.metadata.url=kylin@jdbc,url=jdbc:mysql://mysqldb:3306/kylin?createDatabaseIfNotExist=true,username=hive,password=ChangeMe!,passwordEncrypted=false,maxActive=40,maxIdle=1-,driverClassName=com.mysql.jdbc.Driver" >> $KYLIN_HOME/conf/kylin.properties
