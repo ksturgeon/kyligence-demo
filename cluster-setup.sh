e6962b642807500f22b1ed4eda4ecf7e57f5d333
Kevin Sturgeon <ksturgeon@gmail.com>
2019-03-28 10:57:01 -0400

    added review table and hive mappings

--- cluster-setup.sh
+++ cluster-setup.sh
@@ -17,12 +17,15 @@ maprcli stream topic create -path /demo-streams/tipstream -topic topic1 -partiti
 #Create three tables we'll load later
 maprcli table create -path /demo-tables/business -tabletype json
 maprcli table create -path /demo-tables/user -tabletype json
+maprcli table create -path /demo-tables/review -tabletype json
 
 #Copy public_data source files for our tables
 hadoop fs -copyFromLocal /public_data/common_datasets/yelp/dataset/business.json /demo-files/ 
 hadoop fs -copyFromLocal /public_data/common_datasets/yelp/dataset/user.json /demo-files/ 
+hadoop fs -copyFromLocal /public_data/common_datasets/yelp/dataset/review.json /demo-files/
 
 #load data from public_data
 mapr importJSON -idField business_id -src /demo-files/business.json -dst /demo-tables/business -mapreduce false
 mapr importJSON -idField user_id -src /demo-files/user.json -dst /demo-tables/user -mapreduce false 
+mapr importJSON -idField review_id -src /demo-files/review.json -dst /demo-tables/review -mapreduce false
 
--- hive-table-setup.sql
+++ hive-table-setup.sql
@@ -1,30 +1,46 @@
 DROP TABLE IF EXISTS yelp_business;
 
 CREATE EXTERNAL TABLE yelp_business (
-business_id string,
-address string,
-categories array<string>,
-city string,
-name string,
-review_count int,
-stars int,
-state string)
+business_id string COMMENT 'Business ID',
+address string COMMENT 'Street Address',
+categories array<string> COMMENT 'Business categories',
+city string COMMENT 'Address City',
+name string COMMENT 'Business Name',
+review_count int COMMENT 'Number of reviews',
+stars float COMMENT 'stars rating',
+state string COMMENT 'Address State',
+postal_code string COMMENT 'Address Postal Code')
 STORED BY 'org.apache.hadoop.hive.maprdb.json.MapRDBJsonStorageHandler' 
 TBLPROPERTIES("maprdb.table.name" = "/demo-tables/business","maprdb.column.id" = "business_id");
 
 DROP TABLE IF EXISTS yelp_user;
 
 CREATE EXTERNAL TABLE yelp_user (
-user_id string,
-name string,
-review_count int,
-yelping_since date,
-friends array<string>,
-useful int,
-funny int,
-cool int,
-fans int,
-elite array<string>,
-average_stars float)
+user_id string COMMENT 'User ID',
+name string COMMENT 'First Name',
+review_count int COMMENT 'Number of Reviews',
+yelping_since date COMMENT 'Yelping Since',
+friends array<string> COMMENT 'Friends list',
+useful int COMMENT 'Useful Reviews',
+funny int COMMENT 'Funny Reviews',
+cool int COMMENT 'Cool Reviews',
+fans int COMMENT 'Fans',
+elite array<string> COMMENT 'Elite Reviews',
+average_stars float COMMENT 'Average Stars')
 STORED BY 'org.apache.hadoop.hive.maprdb.json.MapRDBJsonStorageHandler'
 TBLPROPERTIES("maprdb.table.name" = "/demo-tables/user","maprdb.column.id" = "user_id");
+
+DROP TABLE IF EXISTS yelp_review;
+
+CREATE EXTERNAL TABLE yelp_review (
+review_id string COMMENT 'Review ID',
+user_id string COMMENT 'Reviewing User ID',
+business_id string COMMENT 'Business ID',
+stars float COMMENT 'Stars Rating',
+date date COMMENT 'Review Date',
+text string COMMENT 'Review Text',
+useful int COMMENT 'Useful',
+funny int COMMENT 'Funny',
+cool int COMMENT 'Cool')
+STORED BY 'org.apache.hadoop.hive.maprdb.json.MapRDBJsonStorageHandler'
+TBLPROPERTIES("maprdb.table.name" = "/demo-tables/review","maprdb.column.id" = "review_id");
