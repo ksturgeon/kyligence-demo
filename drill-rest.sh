96b1375a22aa5c2046681c42ef3da38f117badf5
Kevin Sturgeon <ksturgeon@gmail.com>
2019-05-03 09:13:07 -0400

    finished drill rest

--- drill-rest.sh
+++ drill-rest.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+#Create the Hive plugin:
+curl --request POST \
+  --url http://mdn:8047/storage/newplugin.json \
+  --header 'Content-Type: application/json' \
+  --data '{"name": "hive","config": {"type": "hive","enabled": true,"configProps": {"hive.metastore.uris": "thrift://hive:9083","javax.jdo.option.ConnectionURL": "jdbc:mysql://:3306;databaseName=../sample-data/drill_hive_db;create=true","hive.metastore.warehouse.dir": "/tmp/drill_hive_wh","fs.default.name": "file:///","hive.metastore.sasl.enabled": "false","datanucleus.schema.autoCreateAll": "true"}}}'
+
+#Enable it:
+curl --request GET \
+  --url http://mdn:8047/storage/hive/enable/true
 
 
 curl --request POST \
-  --url http://dsr-demo-s69kky.se.corp.maprtech.com:8047/query.json \
+  --url http://mdn:8047/query.json \
   --header 'Content-Type: application/json' \
   --data '    {"queryType" : "SQL","query" : "ALTER SYSTEM SET `planner.enable_decimal_data_type` = true"}'
